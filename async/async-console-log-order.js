88c48fe21177cae1c0d2d5bd2d7c47e7a9b2f162
Alexander Olenev <hello@aolenev.me>
2023-02-04 18:06:44 +0200

    live coding

---
+++ async/async-console-log-order.js
@@ -0,0 +1,57 @@
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+}, 100);
+
+setImmediate(() => {
+  console.log(3);
+});
+
+const promise = new Promise((resolve) => {
+  console.log(4);
+
+  resolve(5);
+});
+
+queueMicrotask(() => {
+  console.log(6);
+});
+
+promise
+  .then((result) => {
+    console.log(result);
+
+    return 7;
+  })
+  .then((result) => {
+    console.log(result);
+
+    setTimeout(() => {
+      throw new Error(8);
+    });
+
+    return Promise.resolve(9);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((result) => {
+    console.log(result);
+  });
+
+promise
+  .then((result) => {
+    return new Promise(() => {
+      console.log("1" + result);
+
+      throw new Error(10);
+    });
+  })
+  .catch((result) => {
+    console.log(result.message);
+  });
+
+process.on("uncaughtException", (err) => {
+  console.log(err.message);
+});
---
+++ async/live-coding.md
@@ -0,0 +1,3 @@
+# Live Coding 🧑🏼‍💻
+
+1. [In what order will the numbers be displayed?](./async-console-log-order.js)
