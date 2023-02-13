0b33577600c66f4455dc50b871ed5f1c14c7b0bd
Alexander Olenev <hello@aolenev.me>
2023-02-13 23:36:50 +0200

    minor

--- proto-types/coercions.js
+++ proto-types/coercions.js
@@ -20,7 +20,6 @@ console.log(Object.is(+0, -0));
 console.log(null === undefined);
 console.log(null === null);
 console.log(undefined == undefined);
-console.log(undefined === undefined);
 console.log(Symbol("1") === Symbol("1"));
 console.log({ a: 1 } === { a: 1 });
 console.log({ a: 1 } == { a: 1 });
