14c57212dedd5e149ac1e53e15bee17399a82488
Alexander Olenev <hello@aolenev.me>
2023-02-26 17:48:55 +0200

    added

--- this-function/live-coding.md
+++ this-function/live-coding.md
@@ -2,3 +2,4 @@
 
 - [Block Scope](./block-scope.js)
 - [This and Prototype](./this-and-prototype.js)
+- [Recursion and Currying](./recursion-and-currying.js)
---
+++ this-function/recursion-and-currying.js
@@ -0,0 +1,12 @@
+import assert from "node:assert/strict";
+
+// Implement the function to pass the test.
+
+const add = (number) => {
+  // Write the code only here.
+
+  return number;
+};
+
+assert.deepEqual(add(1)(2)(3)(4)(5)(6)(7)(8)(9)(), 45);
+assert.deepEqual(add(9)(), 9);
